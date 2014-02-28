3e21df9cab9dd95282d6793166b5153451472c58
Mahendra M. Rai <mahemrai@googlemail.com>
2014-02-28 16:21:34 +0000

    Update index.php
    fixed indentation

--- app/routes/index.php
+++ app/routes/index.php
@@ -1,14 +1,14 @@
 <?php
 $app->get('/', function() use ($app) {
-	require '../app/models/Twitter.php';
+    require '../app/models/Twitter.php';
 
-	$twitter = new Twitter();
-	$tweets = $twitter->getHomeTimeline();
+    $twitter = new Twitter();
+    $tweets = $twitter->getHomeTimeline();
 
-	$view_data = array(
-		'tweets' => $tweets
-	);
+    $view_data = array(
+        'tweets' => $tweets
+    );
 	
-	$app->render('home.html.twig', $view_data);
+    $app->render('home.html.twig', $view_data);
 });
 ?>
