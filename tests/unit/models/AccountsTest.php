950c6835068f0a8e987c362a2273881fc7330c3a
Mahendra Rai <mahemrai@googlemail.com>
2014-06-06 00:40:22 +0100

    added Phactory

--- tests/unit/models/AccountsTest.php
+++ tests/unit/models/AccountsTest.php
@@ -1,4 +1,6 @@
 <?php
+require 'app/models/Accounts.php';
+
 /**
  * AccountsTest class
  * @package models
@@ -10,7 +12,7 @@ class AccountsTest extends \PHPUnit_Framework_TestCase {
 
     public function setUp() {
         $this->fixture = array(
-            array(
+            'Twitter' => array(
                 'id' => 1,
                 'account' => 'Twitter',
                 'api_key' => '',
@@ -18,7 +20,7 @@ class AccountsTest extends \PHPUnit_Framework_TestCase {
                 'oauth_token' => '',
                 'oauth_token_secret' => ''
             ),
-            array(
+            'Scoopit' => array(
                 'id' => 2,
                 'account' => 'Scoopit',
                 'api_key' => '',
@@ -27,6 +29,21 @@ class AccountsTest extends \PHPUnit_Framework_TestCase {
                 'oauth_token_secret' => ''
             )
         );
+    }
+
+    public static function setUpBeforeClass() {
+        $pdo = new PDO("sqlite:test.db");
+        Phactory::setConnection($pdo);
+
+        $pdo->exec("
+            CREATE TABLE `sm_accounts` (
+                id INTEGER PRIMARY KEY,
+                account VARCHAR(50),
+                api_key VARCHAR(500),
+                api_secret VARCHAR(500),
+                oauth_token VARCHAR(500),
+                oauth_token_secret VARCHAR(500))
+        ");
     }
 
     public function testGetAllApiAccounts() {
@@ -39,13 +56,32 @@ class AccountsTest extends \PHPUnit_Framework_TestCase {
         $this->assertTrue(!empty($this->fixture));
         $this->assertFalse(empty($this->fixture));
         $this->assertSame($this->fixture, $mock->getAllApiAccounts());
+
+        $account = new Accounts();
+        $data = $account->getAllApiAccounts();
+
+        $this->assertEquals(3, sizeof($data));
     }
 
     public function testGetApiAccount() {
+        $param = 'Scoopit';
+
         $mock = $this->getMock('Accounts', array('getApiAccount'));
 
         $mock->expects($this->any())
              ->method('getApiAccount')
-             ->will($this->returnValue($this->fixture[1]));
+             ->with($param)
+             ->will($this->returnValue($this->fixture[$param]));
+
+        $this->assertSame($this->fixture[$param], $mock->getApiAccount($param));
+        
+        $account = new Accounts();
+        $data = $account->getApiAccount($param);
+
+        $this->assertEquals(3, $data['id']);
+    }
+
+    public function testSaveAccountDetails() {
+
     }
 }
